0dd7234c5b0f107a11ca3cf4f24d27eb29b97e83
HyundooPark <qkrgusen456@gmail.com>
2024-12-26 20:27:46 +0900

    first commit

---
+++ src/main/java/io/hhplus/lectureapply/LectureApplyApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.lectureapply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureApplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureApplyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hhplus/lectureapply/LectureApplyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.lectureapply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
